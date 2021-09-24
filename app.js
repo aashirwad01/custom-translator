35e0434fc237345ff5c1e655a44fcd07a8b1ddac
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2021-09-24 21:51:30 +0530

    feat-added clear button in app

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var txtArea=document.querySelector("#txt-area");
 var btn_getMirror=document.querySelector("#btn-mirror");
 btn_getMirror.addEventListener("click",btnClick);
-
+var btn_clear=document.querySelector("#btn-clear");
+btn_clear.addEventListener("click",btnClear);
 var url="https://tranquil-falls-70811.herokuapp.com/translate/mirror.json";
 
 function urlGenerator(url,text){
@@ -16,13 +17,27 @@ function urlGenerator(url,text){
     txtArea.value="";
     return(finalURL);
     }
-    
-
 
     function btnClick(){
         
         fetch(value())
         .then( response => response.json())
         .then( json => {txtArea.value=json.contents.translated})
+        
         .catch(() => alert("error happended"))
+
+       
     }
+
+    function btnClear(){
+        txtArea.value="";
+        console.log("Done");
+    }
+    
+    
+
+
+   
+
+    // await delay(5000);
+    // txtArea.value="";
